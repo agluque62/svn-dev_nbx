f457647951882a01b1ef6a87e78e9004c5fa7360
Arturo Garcia <arturo.garcia.luque@gmail.com>
2020-04-29 12:23:12 +0200

    Se incluye en la persistencia UnoMasUno los datos asociados a la Habilitación / Deshabiiltación de recursos,

--- Source/RdService/MSTxPersistence.cs
+++ Source/RdService/MSTxPersistence.cs
@@ -11,15 +11,19 @@ using NLog;
 
 using U5ki.Infrastructure;
 using Utilities;
+using System.Runtime.CompilerServices;
+using Lextm.SharpSnmpLib.Pipeline;
 
 namespace U5ki.RdService
 {
     static class LocalExtensions
     {
-        public static string ListToString(this MSTransmittersStatus @this)
+        public static string ListToString(this MSStatus @this)
         {
-            var sb = new StringBuilder($"\n\tMSTransmittersStatus List, Entries {@this.nodes_info.Count()}:");
-            @this.nodes_info.ForEach(node => sb.Append($"\n\t\t{node.txres}, on {node.site}."));
+            var sb = new StringBuilder($"\n\tMSStatus List, Tx Main Entries {@this.main_nodes.Count()}:");
+            @this.main_nodes.ForEach(node => sb.Append($"\n\t\t{node.res}, on {node.site}."));
+            sb.Append($"\n\t               Rx Disabled Entries {@this.disabled_nodes.Count()}:");
+            @this.disabled_nodes.ForEach(node => sb.Append($"\n\t\t{node.res}, on {node.site}."));
             return sb.ToString();
         }
     }
@@ -31,7 +35,7 @@ namespace U5ki.RdService
         {
             RdRegRef = rdReg;
 
-            RdRegRef?.SubscribeToTopic<MSTransmittersStatus>(Identifiers.RdTopic);
+            RdRegRef?.SubscribeToTopic<MSStatus>(Identifiers.RdTopic);
             RdRegRef.ResourceChanged += OnResourceChanged;
             Log.Trace("Modulo Incializado.");
         }
@@ -48,9 +52,9 @@ namespace U5ki.RdService
             var res = DataAccess(() =>
             {
                 /** Borro los dos si estan */
-                Status.nodes_info?.RemoveAll(i => i.txres == main.ID || i.txres == standby.ID);
+                Status.main_nodes?.RemoveAll(i => i.res == main.ID || i.res == standby.ID);
                 /** Añado el Main */
-                Status.nodes_info?.Add(new MSTransmiterInfo() { site = main.Site ?? "none", txres = main.ID });
+                Status.main_nodes?.Add(new MSNodeInfo() { site = main.Site ?? "none", res = main.ID });
                 Log.Trace($"Main Selected {main.ID}. Standby {standby.ID}");
 
                 SaveAndPublish();
@@ -59,17 +63,53 @@ namespace U5ki.RdService
         }
         static public bool IsMain(IRdResource resource)
         {
-            Log.Trace($"Query for {resource.ID}");
+            Log.Trace($"Query Main for {resource.ID}");
             string found = default;
             var res = DataAccess(() =>
             {
-                found = Status.nodes_info.Where(i => i.site == resource.Site && i.txres == resource.ID)
+                found = Status.main_nodes.Where(i => i.site == resource.Site && i.res == resource.ID)
                     .ToList()
                     .Count() > 0 ? "Main" : "StandBy";
 
                 Log.Trace($"Resource {resource.ID} is {found}. {Status.ListToString()}");
             });
             return res ? found == "Main" : false;
+        }
+        static public bool DisableNode(IRdResource resource, bool disable)
+        {
+            Log.Trace($"Disabling node {resource.ID} ==> {disable}");
+            var res = DataAccess(() =>
+            {
+                /** Lo borro de la lista */
+                Status.disabled_nodes?.RemoveAll(i => i.res == resource.ID);
+                /** */
+                if (disable == true)
+                {
+                    Status.disabled_nodes?.Add(new MSNodeInfo() { site = resource.Site ?? "none", res = resource.ID });                
+                    Log.Trace($"Node {resource.ID} Disabled");
+                }
+                else
+                {
+                    Log.Trace($"Node {resource.ID} Enabled");
+                }
+
+                SaveAndPublish();
+            });
+            return res;
+        }
+        static public bool IsNodeDisabled(IRdResource resource)
+        {
+            Log.Trace($"Query Disabled for {resource.ID}");
+            string found = default;
+            var res = DataAccess(() =>
+            {
+                found = Status.disabled_nodes.Where(i => i.site == resource.Site && i.res == resource.ID)
+                    .ToList()
+                    .Count() > 0 ? "Disabled" : "Enabled";
+
+                Log.Trace($"Resource {resource.ID} is {found}. {Status.ListToString()}");
+            });
+            return res ? found == "Disabled" : false;
         }
         /// <summary>
         /// Se supone que a esta rutina se llama en modo MASTER.
@@ -81,15 +121,17 @@ namespace U5ki.RdService
             try
             {
                 /** Leer los datos consolidados */
-                var Info = ServicesHelpers.DeserializeObject<List<MSTransmiterInfo>>(File.ReadAllText(FileName));
-                Status.nodes_info.Clear();
-                Info.ForEach(i => Status.nodes_info.Add(i));
+                //var Info = ServicesHelpers.DeserializeObject<List<MSTransmiterInfo>>(File.ReadAllText(FileName));
+                //Status.nodes_info.Clear();
+                //Info.ForEach(i => Status.nodes_info.Add(i));
+                Status = ServicesHelpers.DeserializeObject<MSStatus>(File.ReadAllText(FileName));
                 Log.Trace($"File loaded. {Status.ListToString()}");
             }
             catch (Exception x)
             {
                 Log.Error("Loading File Exception", x);
-                Status.nodes_info.Clear();
+                Status.main_nodes.Clear();
+                Status.disabled_nodes.Clear();
             }
             /** Ajustar los datos a la nueva configuracion. */
             var allRes = cfg.ConfiguracionUsuarios
@@ -100,8 +142,9 @@ namespace U5ki.RdService
                 .Where(r => r.Tipo == 1 || r.Tipo == 2)
                 .ToList();
 
-            /** Borra de la lista aquellos que han sido eliminadas de configuracion */
-            Status.nodes_info.RemoveAll(n => NotInCfg(allRes, n));
+            /** Borra de las listas aquellos que han sido eliminadas de configuracion */
+            Status.main_nodes.RemoveAll(n => NotInCfg(allRes, n));
+            Status.disabled_nodes.RemoveAll(n => NotInCfg(allRes, n));
             Log.Trace($"List Fixed. {Status.ListToString()}");
 
             SaveAndPublish();
@@ -125,19 +168,20 @@ namespace U5ki.RdService
                     Log.Error("Deserializing Data Exception", x);
                 }
             }
-            else if (e.Type == Identifiers.TypeId(typeof(MSTransmittersStatus)) && e.Content != null)
+            else if (e.Type == Identifiers.TypeId(typeof(MSStatus)) && e.Content != null)
             {
                 try
                 {
-                    Log.Trace($"OnResourceChanged MSTransmittersStatus Event Received.");
+                    Log.Trace($"OnResourceChanged MSStatus Event Received.");
                     MemoryStream ms = new MemoryStream(e.Content);
-                    MSTransmittersStatus MSNodesInfo = Serializer.Deserialize<MSTransmittersStatus>(ms);
+                    MSStatus MSNodesInfo = Serializer.Deserialize<MSStatus>(ms);
                     Log.Trace($"OnResourceChanged Event Received. {MSNodesInfo.ListToString()}");
                     DataAccess(() =>
                     {
                         try
                         {
-                            File.WriteAllText(FileName, ServicesHelpers.SerializeObject(MSNodesInfo.nodes_info));
+                            Status = MSNodesInfo;
+                            File.WriteAllText(FileName, ServicesHelpers.SerializeObject(MSNodesInfo/*.nodes_info*/));
                             Log.Trace($"OnResourceChanged Event Received. Saved to {FileName}");
                         }
                         catch (Exception x)
@@ -157,9 +201,9 @@ namespace U5ki.RdService
             /** Publico los cambios */
             try
             {
-                RdRegRef?.SetValue<MSTransmittersStatus>(Identifiers.RdTopic, "MSTransmittersStatus", Status);
+                RdRegRef?.SetValue<MSStatus>(Identifiers.RdTopic, "MSStatus", Status);
                 RdRegRef?.Publish();
-                File.WriteAllText(FileName, ServicesHelpers.SerializeObject(Status.nodes_info));
+                File.WriteAllText(FileName, ServicesHelpers.SerializeObject(Status/*.nodes_info*/));
                 Log.Trace($"Data Saved and Published. {Status.ListToString()}");
             }
             catch (Exception x)
@@ -177,15 +221,26 @@ namespace U5ki.RdService
             }
             return false;
         }
-        static bool NotInCfg(List<CfgRecursoEnlaceExterno> cfgRecursos, MSTransmiterInfo node)
+        static bool NotInCfg(List<CfgRecursoEnlaceExterno> cfgRecursos, MSNodeInfo node)
         {
-            var inCfg = cfgRecursos.Where(r => r.IdEmplazamiento == node.site && r.IdRecurso == node.txres).ToList();
+            var inCfg = cfgRecursos.Where(r => r.IdEmplazamiento == node.site && r.IdRecurso == node.res).ToList();
             return inCfg.Count() == 0;
         }
         static Logger Log { get => LogManager.GetLogger("MSTxPersistence"); }
         static Registry RdRegRef = null;
         static Semaphore semaphore = new Semaphore(1, 1);
-        static MSTransmittersStatus Status = new MSTransmittersStatus();
+        static MSStatus Status = new MSStatus();
 
+        #region Solo para TEST
+        static public void GenerateResourceChange<T>(T eventdata)
+        {
+            MemoryStream ms = new MemoryStream();
+            Serializer.Serialize(ms, eventdata);
+            byte[] data = ms.ToArray();
+
+            var change = new RsChangeInfo("", "MSStatus", Identifiers.TypeId(typeof(T)), "", data);
+            OnResourceChanged(null, change);
+        }
+        #endregion
     }
 }
